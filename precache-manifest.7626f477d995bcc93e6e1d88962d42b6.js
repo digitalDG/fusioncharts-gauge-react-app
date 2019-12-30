0e403864d0524f353bf1ad1f4b5c31b8e8e109c9
David Gilbert <173236+digitalDG@users.noreply.github.com>
2019-12-29 20:55:34 -0500

    Updates

---
+++ precache-manifest.7626f477d995bcc93e6e1d88962d42b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "351bb43d6dbaa2dea47f1844547d94ea",
+    "url": "/fusioncharts-gauge-react-app/index.html"
+  },
+  {
+    "revision": "ed499b2ef4ae9b4ad367",
+    "url": "/fusioncharts-gauge-react-app/static/css/main.7675890a.chunk.css"
+  },
+  {
+    "revision": "afcad342be004cd3736e",
+    "url": "/fusioncharts-gauge-react-app/static/js/2.362d2f3f.chunk.js"
+  },
+  {
+    "revision": "4658ebc3beb4ccd4d5d959d32725649c",
+    "url": "/fusioncharts-gauge-react-app/static/js/2.362d2f3f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ed499b2ef4ae9b4ad367",
+    "url": "/fusioncharts-gauge-react-app/static/js/main.014ec14e.chunk.js"
+  },
+  {
+    "revision": "76e6152dde12df26a719",
+    "url": "/fusioncharts-gauge-react-app/static/js/runtime-main.3220b28b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fusioncharts-gauge-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
